4698bc636f80d97809976bab0164fb92602fa4cd
Yunus Syed <ysyed4@uic.edu>
2016-07-10 13:10:48 -0500

    condensed

--- pe1.go
+++ pe1.go
@@ -2,39 +2,18 @@ package main
 
 import "fmt"
 
-func three(n int) int {
+func sum(n int) int {
 	total := 0
 
 	for i := 1; i < n; i++ {
-		if i%3 == 0 {
+		if i%3 == 0 || i%5 == 0 {
 			total += i
 		}
 	}
 	return total
 }
 
-func five(n int) int {
-	total := 0
-
-	for i := 1; i < n; i++ {
-		if i%5 == 0 {
-			total += i
-		}
-	}
-	return total
-}
-
-func fifteen(n int) int {
-	total := 0
-
-	for i := 1; i < n; i++ {
-		if i%15 == 0 {
-			total += i
-		}
-	}
-	return total
-}
 func main() {
-	z := three(20) + five(20) - fifteen(20)
+	z := sum(1000)
 	fmt.Println(z)
 }
